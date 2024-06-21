938b19e4e6a069d127f33b3629a4db090b5bb050
Hari Naik <kumavathharinaik@gmail.com>
2024-06-21 12:24:11 +0530

    Calculate Employee Wage And Save Total Wage By Company.

--- src/main/java/EmployeeWage/main.java
+++ src/main/java/EmployeeWage/main.java
@@ -7,6 +7,14 @@ public class main {
     public static final int EMP_RATE_PER_HOUR = 20;
     public static final int NUM_OF_WORKING_DAYS = 2;
     public static final int MAX_HRS_IN_MONTH = 10;
+    public  final String company;
+    public  final int empRatePerHour;
+    public  final int numOfWorkingDays;
+    public  final int maxHoursPerMonth;
+    private static int totalEmpWage;
+
+
+    // ---------------------------- UC_1 checkAttendance ----------------------------------------------
 
     static void checkAttendance(){
         int IS_FULL_TIME = 1;
@@ -17,6 +25,8 @@ public class main {
             System.out.println("Employee is absent.");
         }
     }
+
+    // ---------------------------- UC_2 CalculateDailyWage ----------------------------------------------
 
     static void CalculateDailyWage(){
         int IS_FULL_TIME = 1;
@@ -35,6 +45,8 @@ public class main {
     }
 
 
+    // ---------------------------- UC_3 CalculatePartTimeWage ----------------------------------------------
+
     static void CalculatePartTimeWage(){
         int empHrs = 0;
         int empWage = 0;
@@ -50,6 +62,8 @@ public class main {
         System.out.println("Part-Time Employee Wage : " + empWage);
     }
 
+
+    // ---------------------------- UC_4 CalculateEmployeeWageSwitch ----------------------------------------------
 
     static void CalculateEmployeeWageSwitch() {
         int empHrs = 0;
@@ -72,6 +86,8 @@ public class main {
     }
 
 
+    // ---------------------------- UC_5 CalculateEmployeeMonthWage ----------------------------------------------
+
     static void CalculateEmployeeMonthWage() {
         int empHrs = 0; int empWage = 0; int totalEmpWages = 0;
         for (int day = 0;day < NUM_OF_WORKING_DAYS;day++){
@@ -93,6 +109,8 @@ public class main {
     }
 
 
+
+    // ---------------------------- UC_6 CalculateEmployeeWageTillReachedWorkingDays ----------------------------------------------
 
     static void CalculateEmployeeWageTillReachedWorkingDays() {
             int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
@@ -120,7 +138,9 @@ public class main {
         }
 
 
-        public static int computeEmpWageClaaMethod() {
+    // ---------------------------- UC_7 computeEmpWageClaaMethod ----------------------------------------------
+
+    public static int computeEmpWageClaaMethod() {
             // Variables
             int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 
@@ -148,6 +168,8 @@ public class main {
         }
 
 
+    // ---------------------------- UC_8 MultipleCompany ----------------------------------------------
+
         public static int MultipleCompany(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
             // Variables
             int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
@@ -173,17 +195,81 @@ public class main {
             return totalEmpWage;
         }
 
+        
+
+// ---------------------------- UC_9 EmployeeWageAndSaveTotalWageByCompany ----------------------------------------------
+public main(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+    this.company = company;
+    this.empRatePerHour = empRatePerHour;
+    this.numOfWorkingDays = numOfWorkingDays;
+    this.maxHoursPerMonth = maxHoursPerMonth;
+    this.totalEmpWage = 0;
+}
+
+    public void computeEmpWage() {
+        // variables
+        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+        // Computation
+        while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
+            totalWorkingDays++;
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            totalEmpHrs += empHrs;
+            System.out.println("Days: " + totalWorkingDays + " Emp Hrs: " + empHrs);
+        }
+        totalEmpWage = totalEmpHrs * empRatePerHour;
+        System.out.println("Total Emp Wage for Company: " + company + " is: " + totalEmpWage);
+    }
+
+    @Override
+    public String toString() {
+        return "Total Emp Wage for Company: " + company + " is: " + totalEmpWage;
+    }
+
+
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee wage computation problem");
+        System.out.println("----------------------- UC_1 -----------------------");
         checkAttendance();
+
+        System.out.println("----------------------- UC_2 -----------------------");
         CalculateDailyWage();
+
+        System.out.println("----------------------- UC_3 -----------------------");
         CalculatePartTimeWage();
+
+        System.out.println("----------------------- UC_4 -----------------------");
         CalculateEmployeeWageSwitch();
+
+        System.out.println("----------------------- UC_5 -----------------------");
         CalculateEmployeeMonthWage();
+
+        System.out.println("----------------------- UC_6 -----------------------");
         CalculateEmployeeWageTillReachedWorkingDays();
+
+        System.out.println("----------------------- UC_7 -----------------------");
         computeEmpWageClaaMethod();
+
+        System.out.println("----------------------- UC_8 -----------------------");
         MultipleCompany("DMart", 20, 2, 10);
         MultipleCompany("Reliance", 10, 4, 20);
+
+        System.out.println("----------------------- UC_9 -----------------------");
+        main dMart = new main("DMart", 20, 2, 10);
+        main reliance = new main("Reliance", 10, 4, 20);
+        dMart.computeEmpWage();
+        System.out.println(dMart);
+        reliance.computeEmpWage();
+        System.out.println(reliance);
     }
 }

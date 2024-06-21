18f656dacb8aec23ba75abb8aa9676660ebfaffd
Hari Naik <kumavathharinaik@gmail.com>
2024-06-21 16:54:17 +0530

    Employee Wage Of Multiple Companies Using Interface.

---
+++ src/main/java/EmployeeWage/IComputeEmpWage.java
@@ -0,0 +1,6 @@
+package EmployeeWage;
+
+public interface IComputeEmpWage {
+    void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth);
+    void computeEmpWage();
+}
